c612418257e06bf85ad26d2da3da2c1e7622dcec
Bimal Parajuli <bimalparajuli@Bimals-MacBook-Pro.local>
2017-06-23 17:14:10 -0500

    Temp Api

---
+++ src/main/java/com/api/weather/Weather.java
@@ -0,0 +1,28 @@
+package com.api.weather;
+
+import javax.xml.bind.annotation.*;
+import java.util.List;
+
+@XmlRootElement(name = "dwml")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Weather {
+
+    @XmlElement(name="data")
+    private Data data;
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Weather{");
+        sb.append("data=").append(data);
+        sb.append('}');
+        return sb.toString();
+    }
+}
